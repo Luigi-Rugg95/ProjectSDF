4180fcfd65e1fbbd2060a61ced0ec72c9bebe195
Luigi-Rugg95 <luigi.ruggiero5@studio.unibo.it>
2022-02-28 14:16:11 +0100

    added a random shape generator

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Created on Sun Feb 20 19:04:12 2022
 @author: Luigi
 """
 
+import random as rm
 import numpy as np
 from functions_for_calculating_distances import distance_from_poly, sdf_complex_gradient
 from plotting import plotting_shape
@@ -19,6 +20,18 @@ poly = [
     [0, 1],
     [1, 0.5],
 ]
+
+#Defining a random shape
+
+"""
+poly = [[1,1]]
+
+n=10
+
+for i in range(0,n): 
+    poly.append([np.sqrt(rm.randint(1,5))*np.cos(i*2*np.pi/(n-1)),np.sqrt(rm.randint(1,5))*np.sin(i*2*np.pi/(n-1))])    
+poly=poly[1:]    
+"""
 
 #Points from which we calculate the distance
 X, Y = np.mgrid[-1:3:0.01, -1:3:0.01]
