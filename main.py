a5d2480b8b493875a2a413924e0f987f4e95d435
Luigi-Rugg95 <luigi.ruggiero5@studio.unibo.it>
2022-07-18 10:31:32 +0200

    main modified  load_segmentation

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import numpy as np
 #importing functions
 from plotting import plotting
 from sdf_from_binary_mask import sdf_from_binary_mask as sdf_mask
-
+from sdf_from_binary_mask import load_segmentation
+"""
 segmentation = np.array([
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 1, 1, 1, 0, 0],
@@ -22,8 +23,10 @@ segmentation = np.array([
     [0, 0, 1, 1, 1, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
 ])
+"""
 
+segmentations, files_name = load_segmentation()
 
-twodsdf = sdf_mask(segmentation,0.1) #parameters: segmentation, grid_finess
-
-plotting(twodsdf.sdf(),*twodsdf.grid(), "n") #parameters: sdf, *grid, if "y" it saves the figure
+for segmentation in segmentations:
+    twodsdf = sdf_mask(segmentation,0.1) #parameters: segmentation, grid_finess
+    plotting(twodsdf.sdf(),*twodsdf.grid(), "y") #parameters: sdf, *grid, if "y" it saves the figure
