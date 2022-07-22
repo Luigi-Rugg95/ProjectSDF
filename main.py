6fad5786f2dfc7e8f8459652a31d79460c9598f8
Luigi-Rugg95 <luigi.ruggiero5@studio.unibo.it>
2022-07-22 10:11:30 +0200

    added some comments

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ from plotting import plotting
 from sdf_from_binary_mask import sdf_from_binary_mask as sdf_mask
 from sdf_from_binary_mask import *
 
+#load the segmentation and the name of the input
 segmentations, files_name = load_segmentation()
 
 
 for segmentation,file_name in zip(segmentations,files_name):
     twodsdf = sdf_mask(segmentation,0.1) #parameters: segmentation, grid_finess
-    twodsdf.write_sdf(twodsdf.sdf(), file_name)
+    twodsdf.write_sdf(twodsdf.sdf(), file_name) #file_name is also given in order to keep track of it in the output
     plotting(twodsdf.sdf(),*twodsdf.grid()) #parameters: sdf, *grid
 
